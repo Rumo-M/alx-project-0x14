d16854d96267fcec5ecf1ee0fcda900f3d3746a2
Martha Maloba <marthamarumo92@gmail.com>
2025-08-07 18:55:34 +0200

    Create index.tsx

---
+++ alx-movie-app/pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to ALX Movie App</h1>
+    </div>
+  );
+};
+
+export default HomePage;
